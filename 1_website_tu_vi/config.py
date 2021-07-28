8fe75c40e99bc160f385bd633362b71bc64e131b
SonGoku <son0105goku@gmail.com>
2021-07-28 17:27:31 +0700

    update static and 404.html and xuat_pdf.html

--- 1_website_tu_vi/config.py
+++ 1_website_tu_vi/config.py
@@ -6,3 +6,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')or 'sqlite:///'+os.path.join(basedir,'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    #kết nối database postgres trên Heroku với SQLAlchemy 1.4.x
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
