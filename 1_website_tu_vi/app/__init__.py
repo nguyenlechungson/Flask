d302014fbaf2e97ca7653c845a5393827eeaac50
SonGoku <son0105goku@gmail.com>
2021-07-29 11:37:47 +0700

    update requirement and init.py

--- 1_website_tu_vi/app/__init__.py
+++ 1_website_tu_vi/app/__init__.py
@@ -39,14 +39,14 @@ def create_app(config_class=Config):
             stream_handler.setLevel(logging.INFO)
             app.logger.addHandler(stream_handler)
         else:
-            if not os.path.exists('log'):
-                os.mkdir('log')
-            file_handler= RotatingFileHandler('log/mylog.log',maxBytes=10240,backupCount=10)
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler= RotatingFileHandler('log/myweb.log',maxBytes=10240,backupCount=10)
             file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s: %(lineno)d]'))
             file_handler.setLevel(logging.INFO)
             app.logger.addHandler(file_handler)
         app.logger.setLevel(logging.INFO)
-        app.logger.info('Myblog startup')
+        app.logger.info('MyWeb startup')
     
 
     return app
